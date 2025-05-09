f67b1b46199c42705451047d4b44398668d6ab6b
Joao Patricio <ijpatricio@gmail.com>
2025-05-09 23:17:18 +0100

    Add Teal color to Skim panel

--- src/SkimPanelProvider.php
+++ src/SkimPanelProvider.php
@@ -4,6 +4,7 @@ namespace Ijpatricio\Skim;
 
 use Filament\Panel;
 use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
 use Ijpatricio\Skim\Filament\Pages\Dashboard;
 
 class SkimPanelProvider extends PanelProvider
@@ -14,6 +15,9 @@ class SkimPanelProvider extends PanelProvider
         return $panel
             ->id('skim')
             ->path('skim')
+            ->colors([
+                'primary' => Color::Teal,
+            ])
             ->resources([
                 // ...
             ])
