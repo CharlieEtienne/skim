ba97880f7447f8d681d470bc40c37098c1ceb417
Joao Patricio <ijpatricio@gmail.com>
2025-05-10 01:35:00 +0100

    Add Middleware to Skim Panel

--- src/SkimPanelProvider.php
+++ src/SkimPanelProvider.php
@@ -2,12 +2,20 @@
 
 namespace Ijpatricio\Skim;
 
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Ijpatricio\Skim\Filament\Pages\Dashboard;
 use Ijpatricio\Skim\Filament\Resources\SkimPageResource;
-use Ijpatricio\Skim\Models\SkimPage;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class SkimPanelProvider extends PanelProvider
 {
@@ -30,7 +38,15 @@ class SkimPanelProvider extends PanelProvider
                 // ...
             ])
             ->middleware([
-                // ...
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
             ])
             ->authMiddleware([
                 // ...
