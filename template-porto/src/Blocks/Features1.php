a34130ec4661dfb9458d208fa5df157e06d776b1
Joao Patricio <ijpatricio@gmail.com>
2025-05-20 00:00:02 +0100

    Block Form+Templates work.

--- template-porto/src/Blocks/Features1.php
+++ template-porto/src/Blocks/Features1.php
@@ -3,6 +3,7 @@
 namespace Ijpatricio\SkimTemplatePorto\Blocks;
 
 use Filament\Forms\Components\Builder\Block;
+use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\TextInput;
 use Ijpatricio\Skim\Contracts\HasBlock;
 
@@ -17,35 +18,17 @@ class Features1 implements HasBlock
                 'block_name' => 'Features 1',
             ])
             ->schema([
-                TextInput::make('feature1_icon')
-                    ->label('Feature 1 Icon')
-                    ->default('activity'),
-                TextInput::make('feature1_title')
-                    ->label('Feature 1 Title')
-                    ->default('High experience'),
-                TextInput::make('feature1_description')
-                    ->label('Feature 1 Description')
-                    ->default('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
-
-                TextInput::make('feature2_icon')
-                    ->label('Feature 2 Icon')
-                    ->default('codesandbox'),
-                TextInput::make('feature2_title')
-                    ->label('Feature 2 Title')
-                    ->default('Useful sandboxes'),
-                TextInput::make('feature2_description')
-                    ->label('Feature 2 Description')
-                    ->default('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
-
-                TextInput::make('feature3_icon')
-                    ->label('Feature 3 Icon')
-                    ->default('coffee'),
-                TextInput::make('feature3_title')
-                    ->label('Feature 3 Title')
-                    ->default('Success side projects'),
-                TextInput::make('feature3_description')
-                    ->label('Feature 3 Description')
-                    ->default('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
+                Repeater::make('features')
+                    ->label('Features')
+                    ->schema([
+                        TextInput::make('icon')
+                            ->label('Icon'),
+                        TextInput::make('title')
+                            ->label('Title'),
+                        TextInput::make('description')
+                            ->label('Description'),
+                    ])
+                    ->columns(3),
             ]);
     }
 }
--- template-porto/views/blocks/features1.blade.php
+++ template-porto/views/blocks/features1.blade.php
@@ -4,35 +4,17 @@
     <div class="container max-w-screen-xl mx-auto px-4">
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <div class="bg-gray-50 px-8 py-10 rounded-md">
-                <div class="w-20 py-6 flex justify-center bg-gray-100 rounded-md mb-4">
-                    <i data-feather="{{ $feature1_icon ?? 'activity' }}"></i>
+            @foreach($features ?? [] as $feature)
+                <div class="bg-gray-50 px-8 py-10 rounded-md">
+                    <div class="w-20 py-6 flex justify-center bg-gray-100 rounded-md mb-4">
+                        <i data-feather="{{ data_get($feature, 'icon', 'activity') }}"></i>
+                    </div>
+
+                    <h4 class="font-medium text-gray-700 text-lg mb-4">{{ data_get($feature, 'title', 'High experience') }}</h4>
+
+                    <p class="font-normal text-gray-500 text-md">{{ data_get($feature, 'description', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.') }}</p>
                 </div>
-
-                <h4 class="font-medium text-gray-700 text-lg mb-4">{{ $feature1_title ?? 'High experience' }}</h4>
-
-                <p class="font-normal text-gray-500 text-md">{{ $feature1_description ?? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }}</p>
-            </div>
-
-            <div class="bg-gray-50 px-8 py-10 rounded-md">
-                <div class="w-20 py-6 flex justify-center bg-gray-100 rounded-md mb-4">
-                    <i data-feather="{{ $feature2_icon ?? 'codesandbox' }}"></i>
-                </div>
-
-                <h4 class="font-medium text-gray-700 text-lg mb-4">{{ $feature2_title ?? 'Useful sandboxes' }}</h4>
-
-                <p class="font-normal text-gray-500 text-md">{{ $feature2_description ?? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }}</p>
-            </div>
-
-            <div class="bg-gray-50 px-8 py-10 rounded-md">
-                <div class="w-20 py-6 flex justify-center bg-gray-100 rounded-md mb-4">
-                    <i data-feather="{{ $feature3_icon ?? 'coffee' }}"></i>
-                </div>
-
-                <h4 class="font-medium text-gray-700 text-lg mb-4">{{ $feature3_title ?? 'Success side projects' }}</h4>
-
-                <p class="font-normal text-gray-500 text-md">{{ $feature3_description ?? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }}</p>
-            </div>
+            @endforeach
         </div>
 
     </div>
