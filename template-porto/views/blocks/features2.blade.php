3e32040b4609a246a780196a2800723a05371ced
Joao Patricio <ijpatricio@gmail.com>
2025-05-20 00:30:17 +0100

    Block Form+Templates work.

--- template-porto/views/blocks/features2.blade.php
+++ template-porto/views/blocks/features2.blade.php
@@ -8,71 +8,26 @@
         <p class="font-normal text-gray-500 text-xs md:text-base mb-20">{{ $section_subtitle ?? 'Below is a summary of the places I studied' }}</p>
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            <div class="bg-gray-50 px-8 py-10 rounded-md">
-                <h4 class="font-medium text-gray-700 text-lg mb-4">{{ $feature1_year ?? '2015 – 2016' }}</h4>
+            @foreach($features ?? [] as $feature)
+                <div class="bg-gray-50 px-8 py-10 rounded-md">
+                    <h4 class="font-medium text-gray-700 text-lg mb-4">{{ data_get($feature, 'year', '2015 – 2016') }}</h4>
 
-                <p class="font-normal text-gray-500 text-md mb-4">{{ $feature1_description ?? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }}</p>
+                    <p class="font-normal text-gray-500 text-md mb-4">{{ data_get($feature, 'description', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.') }}</p>
 
-                <div class="relative">
-                    <h6 class="font-semibold text-gray-500 text-md relative z-10">{{ $feature1_link_text ?? 'See the place here' }}</h6>
-                    <span class="w-32 h-1 bg-blue-200 absolute bottom-1 left-0 z-0"></span>
+                    <div class="relative">
+                        <a
+                            href="{{ data_get($feature, 'link_target') }}"
+                            @if(data_get($feature, 'open_in_new_tab'))
+                                target="_blank"
+                            @endif
+                            class="font-semibold text-gray-500 text-md relative z-10"
+                        >
+                            {{ data_get($feature, 'link_text', 'See the place here') }}
+                        </a>
+                        <span class="w-32 h-1 bg-blue-200 absolute bottom-1 left-0 z-0"></span>
+                    </div>
                 </div>
-            </div>
-
-            <div class="bg-gray-50 px-8 py-10 rounded-md">
-                <h4 class="font-medium text-gray-700 text-lg mb-4">{{ $feature2_year ?? '2015 – 2016' }}</h4>
-
-                <p class="font-normal text-gray-500 text-md mb-4">{{ $feature2_description ?? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }}</p>
-
-                <div class="relative">
-                    <h6 class="font-semibold text-gray-500 text-md relative z-10">{{ $feature2_link_text ?? 'See the place here' }}</h6>
-                    <span class="w-32 h-1 bg-blue-200 absolute bottom-1 left-0 z-0"></span>
-                </div>
-            </div>
-
-            <div class="bg-gray-50 px-8 py-10 rounded-md">
-                <h4 class="font-medium text-gray-700 text-lg mb-4">{{ $feature3_year ?? '2015 – 2016' }}</h4>
-
-                <p class="font-normal text-gray-500 text-md mb-4">{{ $feature3_description ?? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }}</p>
-
-                <div class="relative">
-                    <h6 class="font-semibold text-gray-500 text-md relative z-10">{{ $feature3_link_text ?? 'See the place here' }}</h6>
-                    <span class="w-32 h-1 bg-blue-200 absolute bottom-1 left-0 z-0"></span>
-                </div>
-            </div>
-
-            <div class="bg-gray-50 px-8 py-10 rounded-md">
-                <h4 class="font-medium text-gray-700 text-lg mb-4">{{ $feature4_year ?? '2015 – 2016' }}</h4>
-
-                <p class="font-normal text-gray-500 text-md mb-4">{{ $feature4_description ?? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }}</p>
-
-                <div class="relative">
-                    <h6 class="font-semibold text-gray-500 text-md relative z-10">{{ $feature4_link_text ?? 'See the place here' }}</h6>
-                    <span class="w-32 h-1 bg-blue-200 absolute bottom-1 left-0 z-0"></span>
-                </div>
-            </div>
-
-            <div class="bg-gray-50 px-8 py-10 rounded-md">
-                <h4 class="font-medium text-gray-700 text-lg mb-4">{{ $feature5_year ?? '2015 – 2016' }}</h4>
-
-                <p class="font-normal text-gray-500 text-md mb-4">{{ $feature5_description ?? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }}</p>
-
-                <div class="relative">
-                    <h6 class="font-semibold text-gray-500 text-md relative z-10">{{ $feature5_link_text ?? 'See the place here' }}</h6>
-                    <span class="w-32 h-1 bg-blue-200 absolute bottom-1 left-0 z-0"></span>
-                </div>
-            </div>
-
-            <div class="bg-gray-50 px-8 py-10 rounded-md">
-                <h4 class="font-medium text-gray-700 text-lg mb-4">{{ $feature6_year ?? '2015 – 2016' }}</h4>
-
-                <p class="font-normal text-gray-500 text-md mb-4">{{ $feature6_description ?? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }}</p>
-
-                <div class="relative">
-                    <h6 class="font-semibold text-gray-500 text-md relative z-10">{{ $feature6_link_text ?? 'See the place here' }}</h6>
-                    <span class="w-32 h-1 bg-blue-200 absolute bottom-1 left-0 z-0"></span>
-                </div>
-            </div>
+            @endforeach
         </div>
 
     </div>
